6be40e5d3801a79bc150a8de94433a30729f2f94
AdonNeet <baradontap@gmail.com>
2023-06-26 14:42:02 +0700

    minor update

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ A database CRUD implementation from php to MySQL (known as My Squirrel as joke)
 <br>
 
 Note :  
-+ Gunakan XXAMP, karena sudah ada built-in PHP (jangan lupa set PHP di variabel environment pada komputer) 
++ Gunakan XAMPP, karena sudah ada built-in PHP (jangan lupa set PHP di variabel environment pada komputer) 
 + Jangan lupa untuk mendeploy database (market.sql) pada MySQL
 + Jangan lupa untuk set-up connector pada connDB.php 
-+ Clone di dalam file htdocs nya XXAMP
++ Clone di dalam file htdocs nya XAMPP
 + Tambahkan data karyawan secara manual pada database :D
 
 <br>
---
+++ insertStaff.php
@@ -0,0 +1,36 @@
+<?php
+include "connDB.php";
+
+$id_staff = "K01";
+$name = "Azza_228";
+$job = "Kasir";
+$alamat = "earth";
+$pass = "12345678";
+
+$sql = "INSERT INTO karyawan(id_staff, name, job, alamat, pass) VALUES('$id_staff', '$name', '$job','$alamat', '$pass')";
+
+$res = mysqli_query($koneksi, $sql);
+
+$id_staff = "G01";
+$name = "Bara_194";
+$job = "Gudang";
+$alamat = "isekai";
+$pass = "qwertyui";
+
+$sql = "INSERT INTO karyawan(id_staff, name, job, alamat, pass) VALUES('$id_staff', '$name', '$job','$alamat', '$pass')";
+
+$res = mysqli_query($koneksi, $sql);
+
+$id_staff = "G02";
+$name = "Damara_229";
+$job = "Gudang";
+$alamat = "earth";
+$pass = "asdfghjk";
+
+$sql = "INSERT INTO karyawan(id_staff, name, job, alamat, pass) VALUES('$id_staff', '$name', '$job','$alamat', '$pass')";
+
+$res = mysqli_query($koneksi, $sql);
+
+
+
+?>
